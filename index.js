c471b096ac4df9f012fa1f209e9fa2fecbf78e11
Imran Freelance Wala <43759941+Mimranarshad@users.noreply.github.com>
2024-11-08 01:14:01 +0500

    counter-app version 2
    In this version we used different technique.

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let counter = 0;
-let counterValueInput = document.getElementById('counterValueInput');
+let count = document.querySelector(".count");
+let incrementBtn = document.querySelector(".increment");
+let decrementBtn = document.querySelector(".decrement");
+let input = document.querySelector(".counterInput");
+let resetBtn = document.querySelector(".reset-btn");
 
-function updateCounterValue() {
-    document.getElementById('counterValue').innerText = counter;
-}
+incrementBtn.addEventListener("click", (e) => {
+  let inputValue = parseInt(input.value) || 1;
+  let counterText = parseInt(count.innerText) || 0;
+  count.innerText = counterText + inputValue;
+});
 
-function increment() {
-    let incrementValue = parseInt(counterValueInput.value) || 1;
-    counter += incrementValue;
-    updateCounterValue();
-}
-
-function decrement() {
-    let incrementValue = parseInt(counterValueInput.value) || 1;
-    counter -= incrementValue;
-    updateCounterValue()
-}
-
-function reset() {
-    counter = 0;
-    updateCounterValue()
-}
+decrementBtn.addEventListener("click", (e) => {
+    let inputValue = parseInt(input.value) || 1;
+    let counterText = parseInt(count.innerText) || 0;
+    count.innerText = counterText - inputValue;
+});
+  
+resetBtn.addEventListener("click", (e) => {
+    input.value = 0;
+    count.innerText = 0;
+  });
